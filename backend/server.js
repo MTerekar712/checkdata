cccbb7964e08641ec40ea574c705734894fe37e6
MTerekar712 <mayurterekar712@gmail.com>
2026-03-28 15:18:11 +0530

    Initial

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const User = require("./models/User");
+const User = require("./models/user");
 
 const app = express();
 
